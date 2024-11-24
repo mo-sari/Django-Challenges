84a2e4a2e8762881f8c724c0927ebca7f9c02099
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-11-24 11:53:54 +0330

    playing with basic views

--- questions/urls.py
+++ questions/urls.py
@@ -1,5 +1,7 @@
-# from django.urls import path
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', )
-# ]
+urlpatterns = [
+    path('', views.index),
+    path('/<int:question_id>', views.detail),
+]
